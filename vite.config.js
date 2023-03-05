304996f7457ed0c9c8e57ee8c72e9eb50a38d365
IvanChapler <ererer4646@gmail.com>
2023-03-05 17:32:16 +0500

    Buld

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,46 @@ import vuetify from "vite-plugin-vuetify";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vuetify({ autoImport: true })],
+  build: {
+    rollupOptions: {
+      input: {
+        index: "./src/main.js"
+      },
+      // inlineDynamicImports: true,
+      output: {
+        file: 'bundle.js',
+        format: 'cjs',
+      }
+    },
+    // rollupOptions: {
+    //   input: {
+    //     index: "./src/index.js"
+    //   },
+    //   /* single
+    //   output: {
+    //       format: "umd",
+    //       strict: false,
+    //       chunkFileNames: `[name].[hash].js`,
+    //       entryFileNames: "[name].bundle.umd.js",
+    //       dir: "dist"
+    //   },
+    //   */
+    //   // array config example
+    //   // from rollup: export type InternalModuleFormat = 'amd' | 'cjs' | 'es' | 'iife' | 'system' | 'umd';
+    //   output: [
+    //     {
+    //       format: 'cjs',
+    //       entryFileNames: "[name].bundle.[format].js",
+    //     },
+    //     {
+    //       format: 'es',
+    //       entryFileNames: "[name].bundle.[format].js",
+    //     },
+    //     {
+    //       format: 'umd',
+    //       entryFileNames: "[name].bundle.[format].js",
+    //     },
+    //   ]
+    // }
+  },
 });
